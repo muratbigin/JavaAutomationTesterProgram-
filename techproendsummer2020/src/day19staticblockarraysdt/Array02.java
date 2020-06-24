c12de8d6b6f253338be45522fe21ced4b05df4c0
muratbigin <muratbigin00@gmail.com>
2020-06-24 18:51:48 +0200

    Arrays

--- techproendsummer2020/src/day19staticblockarraysdt/Array02.java
+++ techproendsummer2020/src/day19staticblockarraysdt/Array02.java
@@ -6,14 +6,17 @@ public class Array02 {
 
 	public static void main(String[] args) {
 		String arr[] = {"Angile","Leo","Brad","Adam","Jack"};
-		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(arr)); //[Angile, Leo, Brad, Adam, Jack]
 		
 		
 		
 		//-How to sort elements in ascending order
 		
 		Arrays.sort(arr);
-		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(arr));//[Adam, Angile, Brad, Jack, Leo]
+		
+		//For number we use "ascending order", for Strings we use "alphabetical order"
+		//"ascending order"  and  "alphabetical order" both are called "Natural Order"
 
 	}
 
