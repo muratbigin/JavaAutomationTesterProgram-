a36a5e14fafdf074ea0a05b112462af904517149
muratbigin <muratbigin00@gmail.com>
2020-06-24 18:51:33 +0200

    For-String-Arrays

---
+++ techproendsummer2020/src/day19staticblockarraysdt/Array02.java
@@ -0,0 +1,20 @@
+package day19staticblockarraysdt;
+
+import java.util.Arrays;
+
+public class Array02 {
+
+	public static void main(String[] args) {
+		String arr[] = {"Angile","Leo","Brad","Adam","Jack"};
+		System.out.println(Arrays.toString(arr));
+		
+		
+		
+		//-How to sort elements in ascending order
+		
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
