b7f57c9ef8eca83155f2059a09ffe5ed4de0a7e9
muratbigin <muratbigin00@gmail.com>
2020-06-30 00:18:58 +0200

    ListSolutions

---
+++ techproendsummer2020/src/day21listsdt/ListQuestions05.java
@@ -0,0 +1,26 @@
+package day21listsdt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListQuestions05 {
+
+	public static void main(String[] args) {
+		/*
+		 5)Find the size of the list and print the size on the console.
+		 */
+		
+List<Integer> chrList = new ArrayList<>();
+		
+		chrList.add(1);
+		chrList.add(2);
+		chrList.add(3);
+		chrList.add(4);
+		
+		System.out.println(chrList.size());
+		
+		
+
+	}
+
+}
