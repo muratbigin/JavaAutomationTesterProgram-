e5aafcc1e0fa4d409dbf507da74fe61a84376478
muratbigin <muratbigin00@gmail.com>
2020-07-04 23:10:41 +0200

    correct coding

--- java_Practice_Day/src/day_05/ReverseSentence.java
+++ java_Practice_Day/src/day_05/ReverseSentence.java
@@ -13,19 +13,22 @@ public class ReverseSentence {
 	public static void main(String[] args) {
 		
 		
-
-		
-			String input ="I love Java";
-			System.out.println(reverseString(input));
-		}
-		
-		static String reverseString(String input) {
-			String output ="";
-			String[] arr = input.split(" ");
-			for(int w=arr.length-1;w>=0;w--) {
-				output+=arr[w]+" ";
-			}
-			return output;
-		}
+		String str = "I love Java";
+        System.out.println(reverseString(str));
+    }
+    // return = result, return type = result type
+    // A moethod can be VOID or have a RETURN TYPE(String, int, byte, char)
+    // If a method has a return type, Then that method MUST have return keyword at
+    // the end!
+    public static String reverseString(String sentence) {
+        String reversed = "";
+        // split
+        String[] splitted = sentence.split(" ");// [I, love, Java]
+        // splitted[0] splitted[1] splitted[length-1]
+        for (int i = splitted.length - 1; i >= 0; i--) {
+            reversed += splitted[i] + " ";
+        }
+        return reversed;// I always have to have return because this method is not void
+    }
 
 }
