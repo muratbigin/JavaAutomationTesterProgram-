c22c50bbd7f875b57679f378a9c4c18fbf96d3c9
muratbigin <muratbigin00@gmail.com>
2020-07-09 18:42:17 +0200

    ...

--- techproendsummer2020/src/day30polymorphismdt/Questions.java
+++ techproendsummer2020/src/day30polymorphismdt/Questions.java
@@ -1,5 +1,26 @@
 package day30polymorphismdt;
 
 public class Questions {
+	
+	public Integer add() {
+		return 12;
+	}
 
 }
+
+
+class Number extends Questions{
+	
+	/*
+	 When you use wrapper classes as return type in overriding
+	 return types must be same otherwise you will get compile time error.
+	 We need IS-A relationship between the return types in overriding but
+	 for wrapper classes Java did not create parent-child relationship because of that 
+	 you cannot use different wrapper classes for return types in overriding
+	 */
+	
+	public Integer add() {
+		return 1;
+	}
+}
+	
