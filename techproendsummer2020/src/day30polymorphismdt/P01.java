2acf9171c97f6436a3b5182531dbc1a62af6751e
muratbigin <muratbigin00@gmail.com>
2020-07-09 17:23:42 +0200

    Polymorphism

---
+++ techproendsummer2020/src/day30polymorphismdt/P01.java
@@ -0,0 +1,30 @@
+package day30polymorphismdt;
+
+public class P01 {
+	
+	
+	/*
+	 Polymorphism: Poly ==> Multi           Morp==> Form
+	               We will have method in same name but they will be in different form
+	               
+	 Note: Overloading is a "polymorphism"
+	 Note: Overridding is a "polymorphism"
+	 
+	 
+	 Polymorphism = Overloading(static Polymorphism) + Overriding(dynamic Polymorphism)
+	                Compile Time Polymorphism           Run Time Polymorphism
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void add() {
+		
+	}
+	public void add(int i) {
+		
+	}
+
+}
